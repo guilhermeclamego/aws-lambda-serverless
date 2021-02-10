21ad18c882191987f576c049503abdb711b5e44e
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-02-10 15:12:16 -0300

    cadastro de paciente

--- handler.js
+++ handler.js
@@ -18,7 +18,7 @@ module.exports.listarPacientes = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data.Items)
     }
   } catch (err) {
     console.log('Error', err)
@@ -33,18 +33,80 @@ module.exports.listarPacientes = async (event) => {
 }
 
 module.exports.obterPaciente = async (event) => {
-  const { pacienteId } = event.pathParameters;
+  try {
+    const { pacienteId } = event.pathParameters;
 
-  const paciente = pacientes.find((paciente) => paciente.id == pacienteId);
+    const data = await dynamoDb
+      .get({
+        ...params,
+        Key: {
+          paciente_id: pacienteId,
+        },
+      })
+      .promise();
 
-  if(paciente == undefined){
-    return {
-      statusCode: 404,
-      body: JSON.stringify({error: 'Paciente não existe'}, null, 2)
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Paciente não existe" }, null, 2),
+      };
     }
+
+    const paciente = data.Item;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(paciente, null, 2),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(paciente, null, 2),   
-  };
+};
+
+module.exports.cadastrarPaciente = async (event) => {
+  try {
+    const timestamp = new Date().getTime();
+
+    let dados = JSON.parse(event.body);
+
+    const { nome, data_nascimento, email, telefone } = dados;
+
+    const paciente = {
+      paciente_id: uuidv4(),
+      nome,
+      data_nascimento,
+      email,
+      telefone,
+      status: true,
+      criado_em: timestamp,
+      atualizado_em: timestamp,
+    };
+
+    await dynamoDb
+      .put({
+        TableName: "PACIENTES",
+        Item: paciente,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
 };
